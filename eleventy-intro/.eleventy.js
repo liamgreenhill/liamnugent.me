dac71d8e33233e6c7c3fe8630cfbd834767b170b
Liam Nugent <liam@greenhilldigital.com>
2020-04-12 19:32:38 +0100

    ^ That worked! This is a tidy up to remove unnecessary files

--- eleventy-intro/.eleventy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats([
-    "md",
-    "html",
-    "css" // css is not yet a recognized template extension in Eleventy
-  ]);
-};
--- eleventy-intro/posts/blog-post-2.md
+++
@@ -1,6 +0,0 @@
----
-pageTitle: This is my 2nd  Post Title
----
-
-
-This is a paragraph of text in a second blog post.
